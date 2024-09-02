f6834af8c58161ae7b3f7e99f96c648e2b499687
muhammed zaheer <muhdzaheermv@gmail.com>
2024-09-03 04:15:08 +0530

    homepage commit

--- chargespot/landingpage/urls.py
+++ chargespot/landingpage/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('ev_login',views.ev_login,name='ev_login'),
+    path('ev_signup',views.ev_signup,name='ev_signup'),
+    path('ev_station_login',views.ev_station_login,name='ev_station_login'),
+    path('ev_station_signup',views.ev_station_signup,name='ev_station_signup'),
 ]
--- chargespot/landingpage/views.py
+++ chargespot/landingpage/views.py
@@ -2,3 +2,17 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def ev_login(request):
+    return render(request,'ev_login.html')
+
+def ev_signup(request):
+    return render(request,'ev_signup.html')
+
+def ev_station_login(request):
+    return render(request,'ev_station_login.html')
+
+def ev_station_signup(request):
+    return render(request,'ev_station_signup.html')
+
+
